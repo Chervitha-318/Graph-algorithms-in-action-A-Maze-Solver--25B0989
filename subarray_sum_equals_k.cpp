986a2abc0c575c5ac029e7febb5c3c7ac55bc21d
Chervitha-318 <chervithalakkireddy@gmail.com>
2026-05-29 15:30:41 +0530

    Create subarray_sum_equals_k.cpp

---
+++ subarray_sum_equals_k.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+     unordered_map<int,int> mp;
+        mp[0] = 1;
+        int currentSum = 0;
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            currentSum += nums[i];
+            if(mp.count(currentSum - k)) {
+                count += mp[currentSum - k];
+            }
+            mp[currentSum]++;
+        }
+        return count;  
+    }
+};
