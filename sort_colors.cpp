4d07f1820741c7796ba3539f10ab03a0158cbaf9
Chervitha-318 <chervithalakkireddy@gmail.com>
2026-06-08 18:35:05 +0530

    Create sort_colors.cpp

---
+++ sort_colors.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+    int red=0;
+    int white=0;
+    int blue=0;
+    int n=nums.size();
+    for(int i=0;i<n;i++){
+        if(nums[i]==0){
+            red++;
+        }
+        else if(nums[i]==1){
+            white++;
+        }
+        else if(nums[i]==2){
+            blue++;
+        }
+    }
+    for(int i=0;i<red;i++){
+        nums[i]=0;
+    }
+    for(int i=red;i<red+white;i++){
+        nums[i]=1;
+    }
+    for(int i=red+white;i<n;i++){
+        nums[i]=2;
+    }    
+    }
+};
