995ee9a269b44c3ad8c4366c73c2c16f27b9d980
Chervitha-318 <chervithalakkireddy@gmail.com>
2026-06-10 11:59:01 +0530

    Create valid_parenthesis.cpp

---
+++ valid_parenthesis.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isValid(string s) {
+    stack<char> p;
+    for(char c:s){
+        if(c=='(' || c=='[' || c=='{') p.push(c);
+        else if(c==')' || c==']' || c=='}'){
+            if(p.empty()) return false;
+            if((c == ')' && p.top() != '(') || (c == ']' && p.top() != '[') || (c == '}' && p.top() != '{')) return false;
+            p.pop();
+        }
+        }
+    return p.empty();
+    }    
+    
+};
