a2c66282c83a4e5bf247a8343b367b393a5f95c7
Chervitha-318 <chervithalakkireddy@gmail.com>
2026-06-25 11:05:57 +0530

    Create binarytree_inorder_traversal.cpp

---
+++ binarytree_inorder_traversal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    
+    vector<int> answer;
+    void inorder(TreeNode* root) {
+        if (root == nullptr) {
+            return;
+        }
+        inorder(root->left);         
+        answer.push_back(root->val);
+        inorder(root->right);     
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        inorder(root);
+        return answer;
+    }
+};
